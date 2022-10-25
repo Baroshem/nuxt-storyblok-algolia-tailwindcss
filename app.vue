38f71f3f94fbbd368542d4678327fdeb2bec3351
Baroshem <jakub.andrzejewski.dev@gmail.com>
2022-10-25 15:03:02 +0200

    feat: working app

--- app.vue
+++ app.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h2 class="text-2xl">Data From Storyblok</h2>
+    <cat-image-gallery :images="story.content.body"/>
+    <h2 class="text-2xl">Data From Algolia</h2>
+    <cat-image-gallery :images="result.hits"/>
   </div>
 </template>
+
+<script setup lang="ts">
+const { result, search } = useAlgoliaSearch('dxp') // pass your index name as param
+const story = await useAsyncStoryblok("home", { version: "draft" });
+
+onMounted(async () => {
+  await search({ query: 'chill' });
+})
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,25 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxt/image-edge',
+    ["@storyblok/nuxt", {
+      accessToken: process.env.STORYBLOK_ACCESS_TOKEN
+    }],
+    ['@nuxtjs/algolia', {
+      apiKey: process.env.ALGOLIA_API_KEY,
+      applicationId: process.env.ALGOLIA_APPLICATION_ID,
+      indexer: {
+        storyblok: {
+          secret: process.env.INDEXER_SECRET,
+          algoliaAdminApiKey: process.env.ALGOLIA_ADMIN_KEY,
+          indexName: process.env.ALGOLIA_INDEX_NAME,
+          accessToken: process.env.STORYBLOK_ACCESS_TOKEN
+        }
+      }
+    }]
+  ],
+  image: {
+    domains: ['a.storyblok.com']
+  }
 })
